4676b46c7569e0da50f636adbf0db8283d9e65d8
EddEnt <61289536+EddEnt@users.noreply.github.com>
2024-01-21 23:08:54 -0500

    Add project files.

---
+++ BookStoreApp.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace BookStoreApp.API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyHeader().
+                           AllowAnyMethod().
+                           WithOrigins("https://localhost:7266");
+                });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ BookStoreApp.API/Program.cs
@@ -0,0 +1,27 @@
+using BookStoreApp.API.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
